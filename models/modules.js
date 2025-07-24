87b7861e644d90dbd63f66a71a3d56566a353f33
Kayden Mok <kaydenmok.24@ichat.sp.edu.sg>
2025-07-24 23:46:40 +0800

    Practical 4 done

--- models/modules.js
+++ models/modules.js
@@ -1,5 +1,5 @@
-const { PrismaClient, Prisma } = require('@prisma/client'); 
-const prisma = new PrismaClient(); 
+const { PrismaClient, Prisma } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 // Create example
 module.exports.create = function create(code, name, credit) {
@@ -16,11 +16,11 @@ module.exports.create = function create(code, name, credit) {
     }).catch(function (error) {
         // Prisma error codes: https://www.prisma.io/docs/orm/reference/errorreference#p2002             
         // // TODO: Handle Prisma Error, throw a new error if module already exists     
-    if (error instanceof Prisma.PrismaClientKnownRequestError){
-        if (error.code === 'P2002'){
-            throw new Error(`The Module ${code} already exists`);
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2002') {
+                throw new Error(`The Module ${code} already exists`);
+            }
         }
-    }
         throw error;
     });
 };
@@ -36,12 +36,12 @@ module.exports.updateByCode = function updateByCode(code, credit) {
         }
     }).then(function (module) {
         // Leave blank     
-    }).catch(function (error) { 
-        if (error instanceof Prisma.PrismaClientKnownRequestError){
-        if (error.code === 'P2025'){
-            throw new Error(`The Module ${code} does not exist`);
+    }).catch(function (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2025') {
+                throw new Error(`The Module ${code} does not exist`);
+            }
         }
-    }
         throw error;
     });
 }
@@ -54,21 +54,21 @@ module.exports.deleteByCode = function deleteByCode(code, credit) {
         }
     }).then(function (module) {
         // Leave blank     
-    }).catch(function (error) { 
-        if (error instanceof Prisma.PrismaClientKnownRequestError){
-        if (error.code === 'P2025'){
-            throw new Error(`The Module ${code} does not exist`);
+    }).catch(function (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2025') {
+                throw new Error(`The Module ${code} does not exist`);
+            }
         }
-    }
         throw error;
     });
 }
 
 
-module.exports.retrieveAll = function retrieveAll() {     
+module.exports.retrieveAll = function retrieveAll() {
     // TODO: Return all modules  
     return prisma.module.findMany()
-    }; 
+};
 
 
 module.exports.retrieveByCode = function retrieveByCode(code) {
@@ -79,12 +79,91 @@ module.exports.retrieveByCode = function retrieveByCode(code) {
         }
     }).then(function (module) {
         return module
-    }).catch(function (error) { 
-        if (error instanceof Prisma.PrismaClientKnownRequestError){
-        if (error.code === 'P2025'){
-            throw new Error(`The Module ${code} does not exist`);
+    }).catch(function (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2025') {
+                throw new Error(`The Module ${code} does not exist`);
+            }
         }
-    }
         throw error;
     });
 }
+
+// -----------------------------------------------------------------------------------------------------------
+// Chatgtp Examples EXTRAAA
+module.exports.createMany = function createMany(modules) {
+    return prisma.module.createMany({
+        data: modules.map((mod) => ({
+            modCode: mod.code,
+            modName: mod.name,
+            creditUnit: parseInt(mod.credit)
+        })),
+        skipDuplicates: true // avoids error if same modCode already exists
+    }).then(function (result) {
+        return result;
+    }).catch(function (error) {
+        throw error;
+    });
+};
+
+// createMany([
+//     { code: "CS101", name: "Intro to CS", credit: 3 },
+//     { code: "MA101", name: "Math 1", credit: 4 },
+// ])
+
+module.exports.updateMany = function updateMany(oldCredit, newCredit) {
+    return prisma.module.updateMany({
+        where: {
+            creditUnit: parseInt(oldCredit)
+        },
+        data: {
+            creditUnit: parseInt(newCredit)
+        }
+    }).then(function (result) {
+        return result;
+    }).catch(function (error) {
+        throw error;
+    });
+};
+
+// updateMany(3, 5) // Update all modules with creditUnit = 3 to creditUnit = 5
+
+//Upsert - update module if exists, create one if doesnt
+module.exports.upsertByCode = function upsertByCode(code, name, credit) {
+    return prisma.module.upsert({
+        where: {
+            modCode: code
+        },
+        update: {
+            modName: name,
+            creditUnit: parseInt(credit)
+        },
+        create: {
+            modCode: code,
+            modName: name,
+            creditUnit: parseInt(credit)
+        }
+    }).then(function (result) {
+        return result;
+    }).catch(function (error) {
+        throw error;
+    });
+};
+
+
+// upsertByCode("CS105", "Advanced CS", 4)
+
+
+module.exports.deleteMany = function deleteMany(credit) {
+    return prisma.module.deleteMany({
+        where: {
+            creditUnit: parseInt(credit)
+        }
+    }).then(function (result) {
+        return result;
+    }).catch(function (error) {
+        throw error;
+    });
+};
+
+// deleteMany(1) // Deletes all modules with creditUnit = 1
